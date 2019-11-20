286cd576f2af84a2a9368ee2210b9d65d9b4d3fb
Scott Semyan <semyan@hotmail.com>
2019-11-20 10:34:12 -0800

    Adding command line tool to do simple parsing and OCR of PDFs.

---
+++ PdfToText/Program.cs
@@ -0,0 +1,34 @@
+﻿using AzureSearchDocumentClassiferLib;
+using System;
+using System.IO;
+
+namespace PdfToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: PdtToText <FILENAME>");
+                return;
+            }
+
+            string filePath = args[0];
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File {0} does not exist",filePath);
+                return;
+            }
+
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            byte[] fileBytes = PdfHelper.ReadFully(fs);
+            string content = PdfHelper.GetTextFromPdfBytes(fileBytes);
+
+            Console.WriteLine(content);
+
+            File.WriteAllText(filePath + ".txt", content);
+
+        }
+    }
+}
